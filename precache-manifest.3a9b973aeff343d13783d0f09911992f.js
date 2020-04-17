d3c7aa526ca45ddfffe73492173b992a47d34612
giahungbk <giahung197bg@gmail.com>
2020-04-17 12:05:48 +0700

    Updates

---
+++ precache-manifest.3a9b973aeff343d13783d0f09911992f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a215fd8af203d5e74339cdb34ff64bd3",
+    "url": "/modal-lightbox/index.html"
+  },
+  {
+    "revision": "5f08a313cbdda7290b1e",
+    "url": "/modal-lightbox/static/css/main.d175ea8f.chunk.css"
+  },
+  {
+    "revision": "c32c4c0f0bd0d2f51c7f",
+    "url": "/modal-lightbox/static/js/2.8e70e59e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/modal-lightbox/static/js/2.8e70e59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f08a313cbdda7290b1e",
+    "url": "/modal-lightbox/static/js/main.bbc12aa1.chunk.js"
+  },
+  {
+    "revision": "b7077d7a269f12bda340",
+    "url": "/modal-lightbox/static/js/runtime-main.c146cb6b.js"
+  },
+  {
+    "revision": "e5aca032544e5ea7c347559846146e6f",
+    "url": "/modal-lightbox/static/media/img1.e5aca032.jpg"
+  },
+  {
+    "revision": "eea3e601b2c5eb7cb5ce15de6f9034d2",
+    "url": "/modal-lightbox/static/media/img2.eea3e601.jpg"
+  },
+  {
+    "revision": "839029694fc5c2ae56140dba79987403",
+    "url": "/modal-lightbox/static/media/img3.83902969.jpg"
+  }
+]);
